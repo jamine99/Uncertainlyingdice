6955e3b4697d02211c42b4c0be83571b1912b801
Joseph Amine <jamine@stanford.edu>
2020-11-05 15:11:17 -0800

    Added state to number generating functionality

--- agents/QlearningState.py
+++ agents/QlearningState.py
@@ -10,6 +10,27 @@ class state:
         gameMap[8] = bet[1]
         self.gameMap = gameMap
 
+    def generateNumber(self):
+        num = ""
+        for i in range(1,9):
+            num += str(gameMap[i])
+        return int(num)
+
+    #inSANElyEpic
+    def enumerateStateList(self):
+        ret = []
+        for a in range(1,7):
+            for b in range(1,7):
+                for c in range(1,7):
+                    for d in range(1,7):
+                        for e in range(1,7):
+                            for f in range(1,7):
+                                for g in range(1,11):
+                                    currState = ""
+                                    currState += str(a)+str(b)+str(c)+str(d)+str(e)+str(f)+str(g)
+                                    ret+= int(currState)
+        return ret
+
     def saveState(self,filename):
         F = open(filename,"a")
         F.write(self.gameMap)
