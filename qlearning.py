9f246b7320f6fcc1f4fc45c3cbbb8d87dd2fba2b
Joseph Amine <jamine@stanford.edu>
2020-11-05 21:05:09 -0800

    updated statespace creation function

--- qlearning.py
+++ qlearning.py
@@ -19,11 +19,11 @@ def enumerateStateList(self):
     ret = []
     ret.append(-1)
     ret.append(1)
-    for a in range(1,7):
-        for b in range(1,7):
-            for c in range(1,7):
-                for d in range(1,7):
-                    for e in range(1,7):
+    for a in range(0,6):
+        for b in range(0,5-a+1):
+            for c in range(0,5-a-b+1):
+                for d in range(0,5-a-b-c+1):
+                    for e in range(0,5-a-b-c-d+1):
                         for f in range(1,7):
                             for g in range(1,11):
                                 currState = ""
